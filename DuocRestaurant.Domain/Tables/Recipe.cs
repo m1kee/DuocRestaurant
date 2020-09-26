ab86dd3b88cf22bd2737a63f8aa2e818f50633f8
M1kee <michael.nunezb@outlook.com>
2020-09-26 03:45:26 -0300

    recipe tables and ionic login

---
+++ DuocRestaurant.Domain/Tables/Recipe.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain
+{
+    public class Recipe : RestaurantTable
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public string Detail { get; set; }
+        public List<RecipeDetail> RecipeDetails { get; set; }
+    }
+}
---
+++ DuocRestaurant.Domain/Tables/RecipeDetail.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain
+{
+    public class RecipeDetail : RestaurantTable
+    {
+        public int RecipeId { get; set; }
+        public int ProductId { get; set; }
+        public int Count { get; set; }
+        public bool Active { get; set; }
+    }
+}
